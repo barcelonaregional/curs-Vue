bdbd62c2f984c934fb145a53f70202f931664aff
Miquel-BR <miquel.domingo@bcnregional.com>
2021-03-03 12:51:32 +0100

    Create example.vue

---
+++ example.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "example",
+    props: [],
+    components: {},
+    data:function(){
+        return {}
+    },
+    computed: {},
+    watch: {},
+    methods: {},
+    beforeCreate() {},
+    created() {}, // created: function(){}
+    beforeMount(){},
+    mounted() {
+        console.log("MOUNT EXAMPLE");
+    },
+    beforeUpdate(){},
+    updated() {},
+    beforeDestroy() {},
+    destroyed() {},
+}
+</script>
+
+<style scoped  lang="scss">
+mounted, updated, and destroyed
+</style>
